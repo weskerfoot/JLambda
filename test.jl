c566aeb6749fd6f764a3f1c23d0d18e917f44366
Wesley Kerfoot <wjak56@gmail.com>
2013-12-11 21:25:26 -0500

    expanded test program

--- test.jl
+++ test.jl
@@ -27,9 +27,25 @@ def getFile
 def (testUnary n)
   ((-n) + n)
 
+def (splitHelp acc xs ys)
+  if (null xs)
+    then ((reverse acc), ys)
+  else if (null (tail xs))
+    then ((reverse acc), ys)
+  else
+    (splitHelp ((head ys) : acc)
+               (tail (tail xs))
+               (tail ys))
+
+def (splitxs xs)
+  (splitHelp [] xs xs)
+
 def main
   ((print (testUnary 6)) >>
   if False
     then
       undefined
-    else getFile)
+    else (getFile >>= print)
+    >>
+    (print
+      (splitxs "abcdefghijk")))
