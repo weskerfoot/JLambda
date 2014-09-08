cc035dc383c1c4c5a7db01478f880cce73d3eb9d
nisstyre56 <wjak56@gmail.com>
2014-09-08 10:07:55 -0400

    progress on test evaluator

--- vm.js
+++ vm.js
@@ -7,18 +7,36 @@ var pprint = require("./pprint.js");
 
 
 //var istr = fs.readFileSync('/dev/stdin').toString();
-var istr = "lambda a b -> (a + b)";
+var istr = "(a + b)";
 var ast = parse.parseFull(tokenizer.tokenize(istr));
 
-
-function evaluate(ast, environment) {
-  var l = ast.length;
-  for (var i = 0; i < l; i++) {
-    if (ast[i].exprType == "Function") {
-      console.log(ast[i]);
-    }
-  }
+function apply(func, p, environment) {
+  var full_func = evaluate(func, environment);
+  return [full_func, evaluate(p, environment)];
 }
 
-evaluate(ast[0], false);
+function evaluateAll(ast, environment) {
+  var l = ast.length;
+  var evaled = [];
+  for (var i = 0; i < l; i++) {
+    evaled.push(evaluate(ast[i], environment));
+  }
+  return evaled;
+}
+
+function evaluate(ast, environment) {
+  if (ast.exprType == "Application") {
+    return apply(ast.func, ast.p, environment);
+  }
+  else if (ast.exprType === "Unary") { /* Unary function application */
+    return apply(ast.op, ast.val, environment);
+  }
+  else if (ast.exprType === "Name") {
+    return ast.ident;
+  }
+  else {
+    return ast.val;
+  }
+}
+console.log("%j", evaluateAll(ast[0], false));
 
